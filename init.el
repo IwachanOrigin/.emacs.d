dafed9a26b325151a1426b7facec48b2806b2a7a
Yuji Iwanaga <iwanaga@azlab.co.jp>
2020-08-20 19:33:24 +0900

    やっぱりemacsclientを使ったほうが良さそうなので、使うようにした

--- init.el
+++ init.el
@@ -219,6 +219,23 @@
 
 ;; fullscreen mode
 (add-to-list 'default-frame-alist '(fullscreen . fullboth))
+
+;; server start for emacs-client
+(when window-system ; GUI時
+  (require 'server)
+  (unless (eq (server-running-p) 't)
+    (server-start)
+    (defun iconify-emacs-when-server-is-done ()
+      (unless server-clients (iconify-frame)))
+
+    ;; C-x C-cに割り当てる
+    (global-set-key (kbd "C-x C-c") 'server-edit)
+    ;; M-x exitでEmacsを終了できるようにする
+    (defalias 'exit 'save-buffers-kill-emacs)
+    ;; 終了時にyes/noの問い合わせ
+    (setq confirm-kill-emacs 'y-or-n-p)
+  )
+)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
