a4533a524c56e52cf8aed23ad796d932ee12a9c7
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2022-02-27 02:09:05 +0900

    #12 dashboardの設定を見直した。早くなった気がする。

--- init.el
+++ init.el
@@ -249,8 +249,15 @@
 ;; dashboard
 (use-package dashboard
   :ensure t
-  :config
-  (dashboard-setup-startup-hook)
+  :diminish
+  (dashboard-mode page-break-lines-mode)
+  :custom
+  (dashboard-center-content t)
+  (dashboard-items '((recents . 15)))
+  :custom-face
+  (dashboard-heading ((t (:foreground "#f1fa8c" :weight bold))))
+  :hook
+  (after-init . dashboard-setup-startup-hook)
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
