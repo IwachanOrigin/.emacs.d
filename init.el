457822b8ad73adbe186f4e50577b4ef6e0c7f9ab
Yuji Iwanaga <iwanaga@azlab.co.jp>
2023-03-07 12:14:19 +0900

    centaur-tabs の設定を詳細にした

--- init.el
+++ init.el
@@ -369,10 +369,53 @@ _M-C-p_: 前の括弧始まりへ移動
 (use-package centaur-tabs
   :demand
   :config
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-height 32)
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-set-bar 'over)
+  (setq centaur-tabs-set-modified-marker t)
+  (centaur-tabs-headline-match)
   (centaur-tabs-mode t)
+  (defun centaur-tabs-buffer-groups ()
+    "`centaur-tabs-buffer-groups' control buffers' group rules.
+ Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+ All buffer name start with * will group to \"Emacs\".
+ Other buffer group by `centaur-tabs-get-group-name' with project name."
+    (list
+     (cond
+	    ((or (string-equal "*" (substring (buffer-name) 0 1))
+	         (memq major-mode '(magit-process-mode
+				                      magit-status-mode
+				                      magit-diff-mode
+				                      magit-log-mode
+				                      magit-file-mode
+				                      magit-blob-mode
+				                      magit-blame-mode)))
+	     "Emacs")
+	    ((derived-mode-p 'prog-mode)
+	     "Editing")
+	    ((derived-mode-p 'dired-mode)
+	     "Dired")
+	    ((memq major-mode '(helpful-mode
+			                    help-mode))
+	     "Help")
+	    ((memq major-mode '(org-mode
+			                    org-agenda-clockreport-mode
+			                    org-src-mode
+			                    org-agenda-mode
+			                    org-beamer-mode
+			                    org-indent-mode
+			                    org-bullets-mode
+			                    org-cdlatex-mode
+			                    org-agenda-log-mode
+			                    diary-mode))
+	     "OrgMode")
+	    (t
+	     (centaur-tabs-get-group-name (current-buffer))))))
   :bind
-  ("C-<prior>" . centaur-tabs-backward)
-  ("C-<next>" . centaur-tabs-forward))
+  ("C-." . centaur-tabs-backward)
+  ("C-," . centaur-tabs-forward)
+  ("C-c t" . centaur-tabs-counsel-switch-group))
 
 ;; default run treemacs
 (add-hook 'emacs-startup-hook 'treemacs)
