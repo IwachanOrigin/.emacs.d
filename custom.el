d0f545cb9bcd694ab69ad78346ad18945b98dab3
iwanaga <iwanaga@azlab.co.jp>
2023-07-14 12:17:32 +0900

    add custom.el. When improving the init.el, generated.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dired-sidebar centaur-tabs restart-emacs hydra cmake-mode ivy-migemo migemo markdown-mode counsel hungry-delete all-the-icons company projectile editorconfig glsl-mode tr-ime))
+ '(sort-fold-case t t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
