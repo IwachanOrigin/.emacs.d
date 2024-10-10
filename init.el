98cceddb0dfebb44a529971d8a772f22761a0f20
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2024-10-10 13:34:33 +0900

    [init] Add cape package.

--- init.el
+++ init.el
@@ -154,11 +154,6 @@
   :init
   (global-corfu-mode))
 
-;; Orderless
-(use-package orderless
-  :ensure t
-  :defer 2)
-
 ;; Support corfu popup
 (use-package corfu-popupinfo
   :after corfu
@@ -166,6 +161,33 @@
   :hook (corfu-mode . corfu-popupinfo-mode)
   :config
   (setq corfu-popupinfo-delay 0.2)) ;; popup delay time
+
+;; Add extensions of corfu
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative keys: M-p, M-+, ...
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
+
+;; Orderless
+(use-package orderless
+  :ensure t
+  :defer 2)
 
 ;; font settings
 (when (member "UDEV Gothic NF" (font-family-list))
