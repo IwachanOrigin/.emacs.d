fc0d4d8d2a528bd0afd58be7845a4e65ff6ab0cb
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2026-02-02 11:14:53 +0900

    [init.org] Updated. I use msys2 ucrt64 emacs binary file.

--- custom.el
+++ custom.el
@@ -4,16 +4,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-snippet lsp-mode fontaine ef-themes apheleia rg vundo undo-fu-session undo-fu which-key difftastic diff-hl magit tempel-collection tempel symbol-overlay eglot-signature-eldoc-talkative eldoc-box eglot-x eglot-booster consult-eglot eglot-tempel ace-window slime-company slime web-beautify emmet-mode jtsx typescript-mode highlight-quoted highlight-defined imenu-list rainbow-delimiters breadcrumb perfect-margin aggressive-indent beframe spacious-padding goggles pulsar lin avy go-translate string-inflection puni modus-themes corfu-prescient vertico-prescient prescient vertico-truncate nerd-icons-corfu nerd-icons-dired nerd-icons-completion nerd-icons org-modern-indent org-modern vc-use-package swiper faceup bind-key cl-generic cl-lib eglot eldoc erc external-completion flymake idlwave jsonrpc let-alist map nadvice ntlm xref olivetti cape embark-consult embark marginalia consult vertico orderless corfu migemo treesit-auto treesit tree-sitter-langs tree-sitter verilog-mode use-package tramp tr-ime svg soap-client so-long seq restart-emacs python projectile project org markdown-mode ivy-migemo hydra hungry-delete glsl-mode editorconfig dired-sidebar dashboard counsel company cmake-mode centaur-tabs all-the-icons))
+   '(breadcrumb cape clang-format cmake-build cmake-mode consult-eglot
+                corfu dape dape-cmake dart-mode diff-hl difftastic
+                eglot-java eglot-signature-eldoc-talkative eglot-x
+                eldoc-box fontaine hydra keycast marginalia
+                markdown-mode migemo nerd-icons-completion
+                nerd-icons-corfu nerd-icons-dired orderless org-modern
+                org-modern-indent rainbow-delimiters spacious-padding
+                tr-ime undo-fu undo-fu-session vc-use-package
+                vertico-truncate vundo web-mode welcome-dashboard
+                yaml-mode))
  '(package-vc-selected-packages
-   '((lsp-snippet :vc-backend Git :url "https://github.com/svaante/lsp-snippet")
-     (eglot-x :vc-backend Git :url "https://github.com/nemethf/eglot-x")
-     (eglot-booster :vc-backend Git :url "https://github.com/jdtsmith/eglot-booster")
-     (breadcrumb :vc-backend Git :url "https://github.com/joaotavora/breadcrumb")
-     (vertico-truncate :vc-backend Git :url "https://github.com/jdtsmith/vertico-truncate")
-     (nerd-icons-corfu :vc-backend Git :url "https://github.com/LuigiPiucco/nerd-icons-corfu")
-     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package")
-     (org-modern-indent :vc-backend Git :url "https://github.com/jdtsmith/org-modern-indent")))
+   '((org-modern-indent :vc-backend Git :url
+                        "https://github.com/jdtsmith/org-modern-indent")
+     (dape-cmake :vc-backend Git :url
+                 "https://github.com/IwachanOrigin/dape-cmake")
+     (cmake-build :vc-backend Git :url
+                  "https://github.com/IwachanOrigin/cmake-build")
+     (eglot-x :vc-backend Git :url
+              "https://github.com/nemethf/eglot-x")
+     (breadcrumb :vc-backend Git :url
+                 "https://github.com/joaotavora/breadcrumb")
+     (vertico-truncate :vc-backend Git :url
+                       "https://github.com/jdtsmith/vertico-truncate")
+     (welcome-dashboard :vc-backend Git :url
+                        "https://github.com/konrad1977/welcome-dashboard")
+     (nerd-icons-corfu :vc-backend Git :url
+                       "https://github.com/LuigiPiucco/nerd-icons-corfu")
+     (vc-use-package :vc-backend Git :url
+                     "https://github.com/slotThe/vc-use-package")
+     (lsp-snippet :vc-backend Git :url
+                  "https://github.com/svaante/lsp-snippet")))
  '(sort-fold-case t t)
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
@@ -21,21 +42,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t :background "#fff6d8")))
- '(header-line ((t :box (:line-width 4 :color "#f5e9cb" :style nil))))
- '(header-line-highlight ((t :box (:color "#484431"))))
- '(keycast-key ((t)))
- '(line-number ((t :background "#fff6d8")))
- '(mode-line ((t :background "#fff6d8" :overline "#403328" :box (:line-width 6 :color "#fff6d8" :style nil))))
- '(mode-line-active ((t :background "#fff6d8" :overline "#403328" :box (:line-width 6 :color "#fff6d8" :style nil))))
- '(mode-line-highlight ((t :box (:color "#484431"))))
- '(mode-line-inactive ((t :background "#fff6d8" :overline "#c5baa6" :box (:line-width 6 :color "#fff6d8" :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "#fff6d8" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#c7b7a6" :style nil))))
- '(tab-line-tab ((t)))
- '(tab-line-tab-active ((t)))
- '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "#fff6d8" :foreground "#fff6d8")))
- '(window-divider ((t (:background "#fff6d8" :foreground "#fff6d8"))))
- '(window-divider-first-pixel ((t (:background "#fff6d8" :foreground "#fff6d8"))))
- '(window-divider-last-pixel ((t (:background "#fff6d8" :foreground "#fff6d8")))))
+ )
