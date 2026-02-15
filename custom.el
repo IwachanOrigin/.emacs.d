fc5411903c870eb16a0134efa9c2f4a79f09f999
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2026-02-15 21:39:17 +0900

    [init, readme] Changed file. removed init.org. Added readme.org. init.el will generate readme.el every time.

--- custom.el
+++ custom.el
@@ -3,38 +3,40 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doom-modeline-check-simple-format nil nil nil "Customized with use-package doom-modeline")
  '(package-selected-packages
-   '(breadcrumb cape clang-format cmake-build cmake-mode consult-eglot
-                corfu dape dape-cmake dart-mode diff-hl difftastic
-                eglot-java eglot-signature-eldoc-talkative eglot-x
-                eldoc-box fontaine hydra keycast marginalia
-                markdown-mode migemo nerd-icons-completion
-                nerd-icons-corfu nerd-icons-dired orderless org-modern
-                org-modern-indent rainbow-delimiters spacious-padding
-                tr-ime undo-fu undo-fu-session vc-use-package
-                vertico-truncate vundo web-mode welcome-dashboard
-                yaml-mode))
+   '(breadcrumb cape clang-format cmake-build cmake-mode
+		consult-compile-multi consult-eglot corfu dape
+		dape-cmake dart-mode dashboard diff-hl difftastic
+		dirvish doom-modeline eglot-java
+		eglot-signature-eldoc-talkative eglot-x eldoc-box
+		fontaine gnu-elpa-keyring-update helpful
+		hide-mode-line hl-todo indent-bars keycast marginalia
+		markdown-mode migemo mini-frame minions
+		nerd-icons-completion nerd-icons-corfu
+		nerd-icons-dired orderless org-modern
+		org-modern-indent projection-multi rainbow-delimiters
+		rg solaire-mode spacious-padding tr-ime
+		treemacs-nerd-icons undo-fu undo-fu-session
+		vc-use-package vertico-truncate vundo web-mode
+		welcome-dashboard which-key-posframe yaml-mode))
  '(package-vc-selected-packages
    '((org-modern-indent :vc-backend Git :url
-                        "https://github.com/jdtsmith/org-modern-indent")
+			"https://github.com/jdtsmith/org-modern-indent")
      (dape-cmake :vc-backend Git :url
-                 "https://github.com/IwachanOrigin/dape-cmake")
+		 "https://github.com/IwachanOrigin/dape-cmake")
      (cmake-build :vc-backend Git :url
-                  "https://github.com/IwachanOrigin/cmake-build")
+		  "https://github.com/IwachanOrigin/cmake-build")
      (eglot-x :vc-backend Git :url
-              "https://github.com/nemethf/eglot-x")
+	      "https://github.com/nemethf/eglot-x")
      (breadcrumb :vc-backend Git :url
-                 "https://github.com/joaotavora/breadcrumb")
+		 "https://github.com/joaotavora/breadcrumb")
      (vertico-truncate :vc-backend Git :url
-                       "https://github.com/jdtsmith/vertico-truncate")
-     (welcome-dashboard :vc-backend Git :url
-                        "https://github.com/konrad1977/welcome-dashboard")
+		       "https://github.com/jdtsmith/vertico-truncate")
      (nerd-icons-corfu :vc-backend Git :url
-                       "https://github.com/LuigiPiucco/nerd-icons-corfu")
+		       "https://github.com/LuigiPiucco/nerd-icons-corfu")
      (vc-use-package :vc-backend Git :url
-                     "https://github.com/slotThe/vc-use-package")
-     (lsp-snippet :vc-backend Git :url
-                  "https://github.com/svaante/lsp-snippet")))
+		     "https://github.com/slotThe/vc-use-package")))
  '(sort-fold-case t t)
  '(warning-suppress-types '((use-package))))
 (custom-set-faces
--- early-init.el
+++ early-init.el
@@ -6,134 +6,26 @@
 
 ;; GC
 (setq gc-cons-threshold most-positive-fixnum)
-;; For Emacs 27+
-(setq package-enable-at-startup nil
-      package-quickstart nil)
-;; Always load newest byte code
-(setq load-prefer-newer t)
 
-;; menu bar false
-(push '(menu-bar-lines . nil) default-frame-alist)
-;; tool bar false
-(push '(tool-bar-lines . nil) default-frame-alist)
-;; scroll bar false
-(push '(scroll-bar-mode . nil) default-frame-alist)
-;; vertical scroll bars false
-(push '(vertical-scroll-bars . nil) default-frame-alist)
-;; display fullscreen
-;;(push '(fullscreen . maximized) default-frame-alist)
-;; No implicit resizing
-(setq frame-inhibit-implied-resize t)
-;; No use file select dialog
-(setq use-file-dialog nil)
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+;; Defer garbage collection further back in the startup process
+(setopt gc-cons-threshold most-positive-fixnum
+        gc-cons-percentage 0.6
 
-;; yes or no to y or n
-(if (version<= "27.0.00" emacs-version)
-    (setq use-short-answers t)
-  (defalias 'yes-or-no-p #'y-or-n-p))
+        ;; In noninteractive sessions, prioritize non-byte-compiled source files to
+        ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+        ;; to skip the mtime checks on every *.elc file.
+        load-prefer-newer noninteractive
 
-;; quiet start
-(setq inhibit-startup-message t)
-(setq inhibit-startup-screen t)
-(setq inhibit-startup-echo-area-message t)
-;; No use x resources
-(setq inhibit-x-resources t)
-(defun display-startup-echo-area-message ()
-  (message ""))
+        ;; `use-package' is builtin since 29.
+        ;; It must be set before loading `use-package'.
+        use-package-enable-imenu-support t)
+(setq file-name-handler-alist nil)
 
-;; Displaying Blank Characters
-(setq-default show-trailing-whitespace t)
-(set-face-background 'trailing-whitespace "#b14770")
-;; Explicit end of buffer
-(setq-default indicate-empty-lines t)
-(setq-default indicate-buffer-boundaries 'left)
-
-;; default tab-width 2
-(setq-default tab-width 2)
-;; Use spaces for tabs
-(setq-default indent-tabs-mode nil)
-
-;; No automatic save list file is created.
-(setq auto-save-list-file-prefix nil)
-;; Do not back up
-(setq backup-inhibited t)
-;; No autosave files are created.
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-;; Delete auto-save file on exit
-(setq delete-auto-save-files t)
-;; Disable lockfiles. i.e. #foo.txt
-(setq create-lockfiles nil)
-;; Turn off beep and flash
-(setq ring-bell-function 'ignore)
-
-;; Highlight cursor line
-(global-hl-line-mode t)
-
-;; Automatically pair parentheses
-(electric-pair-mode 1)
-
-;; Ignore case when sorting
-(custom-set-variables
- '(sort-fold-case t t))
-
-;; Always add a newline automatically at the end of a file while saving
-(setq-default require-final-newline t)
-
-;; cu, cuh
-(add-to-list 'auto-mode-alist '("\\.cu\\'" . c-mode))
-(add-to-list 'auto-mode-alist '("\\.cuh\\'" . c-mode))
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.c\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cc\\'" . c++-mode))
-
-;; line number
-(when (version<= "26.0.50" emacs-version)
-  (global-display-line-numbers-mode t))
-
-;; column number off
-(column-number-mode nil)
-
-;; Output custom setting to custom.el.
-;; i.e. (custom-set-variables~~~ (custom-set-faces~~~~
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file t)
-
-;; Associate extensions to be used with ff-find-other-file
-(setq cc-other-file-alist
-  '(
-   ("\\.c"   (".h" ".hpp"))
-   ("\\.cpp"   (".h" ".hpp"))
-   ("\\.h"   (".c" ".cpp"))
-   ("\\.hpp"   (".c" ".cpp"))))
-
-;; Set target directories to look for with ff-find-other-files
-(setq ff-search-directories
-  '("." "../src" "../include" "../main" "../core" "../*"))
-
-;; Set ff-find-other-file to work with Meta+t
-(global-set-key "\M-t" 'ff-find-other-file)
-;; Change "Ctrl + h" to backspace
-(global-set-key "\C-h" `delete-backward-char)
-;; Delete an entire line with c-k
-(setq kill-whole-line t)
-
-;; Highlight matching pairs of parentheses.
-(setq show-paren-delay 0)
-(show-paren-mode)
-
-;; LSP-mode setting
-;; https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
-(setenv "LSP_USE_PLISTS" "true")
-
-;; Set scratch buffer screen
-(setq initial-scratch-message
-      ";;
-;; Initialize dashboard...
-;;
-")
-
-(provide 'early-init)
+;; For android
+(when (eq system-type 'android)
+  (setenv "PATH" (format "%s:%s" "/data/data/com.termux/files/usr/bin"
+                         (getenv "PATH")))
+  (push "/data/data/com.termux/files/usr/bin" exec-path)
+  (setopt image-scaling-factor 3))
 
